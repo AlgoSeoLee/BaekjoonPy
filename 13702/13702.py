48e2a8b702cc80d21676d16fdbfcdea6a732e329
tpdns90321 <tpdns9032100@gmail.com>
2021-03-04 14:32:55 +0900

    Solved 13702

---
+++ 13702/13702.py
@@ -0,0 +1,29 @@
+from sys.stdin import stdin
+
+"https://www.acmicpc.net/problem/13702 이상한 술집 <실버 3>"
+
+N, target = map(int, stdin.readline().rstrip().split())
+
+lines = []
+for _ in range(N):
+    lines.append(int(stdin.readline()))
+
+start = 1
+end = max(lines)
+length = 0
+while start <= end:
+    current = (start + end) // 2
+
+    cut = 0
+    for l in lines:
+        cut = cut + l // current
+
+    if cut >= target:
+        if current > length:
+            length = current
+
+        start = current + 1
+    else:
+        end = current - 1
+
+print(length)
