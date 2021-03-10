f5ad9a1e059ab0b8ad27c7d81a44e059a9af8496
tpdns90321 <tpdns9032100@gmail.com>
2021-03-10 13:26:05 +0000

    add_comment: 파일 이름 규칙 추가

--- add_comment.py
+++ add_comment.py
@@ -1,3 +1,25 @@
 import sys
+import json
+import re
 
-print(sys.argv)
+NAME_RULE = r"(\d+)/\d+.py"
+
+adding_files = json.loads(sys.argv[1])
+for path in adding_files:
+    is_solve = re.match(NAME_RULE, path)
+    if not is_solve:
+        continue
+
+    regs = is_solve.regs[1]
+    problem = int(path[regs[0]:regs[1]])
+
+    print("commenting: " + path)
+    with open(path, "w") as target:
+        end_of_import = 0
+        where = 1
+        while end_of_import == 0:
+            line = target.readline()
+            if line.find('import') <= 0:
+                end_of_import = where
+            else:
+                where = where + 1
