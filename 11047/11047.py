352d8d83d7d7311d106255ff071614b3048e30a5
tpdns90321 <tpdns9032100@gmail.com>
2021-03-11 05:04:23 +0000

    remove tab in 11047.py

--- 11047/11047.py
+++ 11047/11047.py
@@ -6,11 +6,11 @@ types, won = map(int, stdin.readline().split())
 
 coins = []
 for _ in range(types):
-	coins.append(int(stdin.readline().rstrip()))
+    coins.append(int(stdin.readline().rstrip()))
 
 each = 0
 for c in sorted(coins, reverse=True):
-	each = each + won // c
-	won = won % c
+    each = each + won // c
+    won = won % c
 
 print(each)
