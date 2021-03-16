240177b9966c10b784f952f23e3c2a2cc0070433
tpdns90321 <tpdns9032100@gmail.com>
2021-03-16 14:48:23 +0000

    Failed 5525

---
+++ 5525/5525.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+ioi = int(stdin.readline())
+length = int(stdin.readline())
+p = stdin.readline()
+
+result = 0
+cur = 0
+limit = length - ioi * 2
+condition = 'I' + 'OI' * ioi
+while cur < limit:
+    if p[cur] == 'I' and p[cur:cur+2*ioi+1] == condition:
+        result += 1
+        cur += 2
+    else:
+        cur += 1
+
+print(result)
+
