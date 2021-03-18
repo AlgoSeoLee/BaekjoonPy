7d010963f25a240db4d03623aa76797f478395d4
tpdns90321 <tpdns9032100@gmail.com>
2021-03-18 13:35:58 +0000

    Failed 3865

---
+++ 3865/3865.py
@@ -0,0 +1,34 @@
+from sys import stdin
+import re
+
+SPLIT = re.compile(r"[:,]")
+
+while True:
+    number_of_academy = int(stdin.readline())
+    if number_of_academy == 0:
+        break
+
+    academies = {}
+    target = None
+    for _ in range(number_of_academy):
+        line = stdin.readline().rstrip()[:-1]
+
+        academy = SPLIT.split(line)
+        academy_name = academy[0]
+        academy = academy[1:]
+
+        if len(academies) == 0:
+            target = academy_name
+        academies[academy_name] = set(academy)
+
+    result = set()
+    queue = academies[target]
+    while len(queue) != 0:
+        name = queue.pop()
+        try:
+            next_academy = academies[name]
+            queue = queue.union(next_academy)
+        except KeyError:
+            result.add(name)
+
+    print(len(result))
