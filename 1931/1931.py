30206ca249653b0c4d706dd8e780b17dd45876bc
tpdns90321 <tpdns9032100@gmail.com>
2021-03-26 23:31:54 +0900

    Failed 1931

---
+++ 1931/1931.py
@@ -0,0 +1,33 @@
+from sys import stdin
+from time import sleep
+
+num_of_meeting = int(stdin.readline())
+meetings = dict()
+
+for _ in range(num_of_meeting):
+    start, end = tuple(map(int, stdin.readline().split()))
+    if meetings.get(start) is None:
+        meetings[start] = end
+
+table = []
+end_of_table = max(meetings.keys())
+
+for i in sorted(meetings.keys()):
+    result = 1
+    start = i
+    end = meetings[start]
+
+    while start < end_of_table:
+        start = end+1
+        while meetings.get(start) is None and start < end_of_table:
+            start += 1
+
+        try:
+            end = meetings[start]
+            result += 1
+        except KeyError:
+            pass
+
+    table.append(result)
+
+print(max(table))
