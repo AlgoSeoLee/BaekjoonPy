3b888f4a912e0127ada59b3085a630b63bade649
tpdns90321 <tpdns9032100@gmail.com>
2021-03-27 13:49:36 +0000

    Solved 1931

--- 1931/1931.py
+++ 1931/1931.py
@@ -1,34 +1,21 @@
 from sys import stdin
+from operator import itemgetter
 
 "https://www.acmicpc.net/problem/1931 회의실 배정 <Silver II>"
 
 num_of_meeting = int(stdin.readline())
-meetings = dict()
+meetings = []
 
 for _ in range(num_of_meeting):
-    start, end = tuple(map(int, stdin.readline().split()))
-    if meetings.get(start) is None:
-        meetings[start] = end
+    meetings.append(tuple(map(int, stdin.readline().split())))
 
-table = []
-end_of_table = max(meetings.keys())
+meetings.sort(key=itemgetter(1,0))
 
-for i in sorted(meetings.keys()):
-    result = 1
-    start = i
-    end = meetings[start]
+count = 0
+end = 0
+for m in meetings:
+    if m[0] >= end:
+        count += 1
+        end = m[1]
 
-    while start < end_of_table:
-        start = end+1
-        while meetings.get(start) is None and start < end_of_table:
-            start += 1
-
-        try:
-            end = meetings[start]
-            result += 1
-        except KeyError:
-            pass
-
-    table.append(result)
-
-print(max(table))
+print(count)
