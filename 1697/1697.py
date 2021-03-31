b18a35aeebbf3a9e080190bd489a81a3863c7c49
tpdns90321 <tpdns9032100@gmail.com>
2021-03-31 12:23:41 +0000

    Solved 1697

---
+++ 1697/1697.py
@@ -0,0 +1,33 @@
+from sys import stdin
+
+def hide_and_seek(origin, target):
+    queue = []
+    visited = [False for i in range(100001)]
+
+    if origin == target:
+        return 0
+    queue.append((origin, 0))
+    visited[origin] = True
+
+    while len(queue) != 0:
+        position, current = queue.pop()
+
+        new_positions = []
+
+        if position != 0:
+            new_positions.append(position - 1)
+        if position != 100000:
+            new_positions.append(position + 1)
+        if position <= 50000:
+            new_positions.append(position * 2)
+
+        ops = current + 1
+        for p in new_positions:
+            if p == target:
+                return ops
+            elif not visited[p]:
+                visited[p] = True
+                queue.insert(0, (p, ops))
+
+origin, target = map(int, stdin.readline().split())
+print(hide_and_seek(origin, target))
