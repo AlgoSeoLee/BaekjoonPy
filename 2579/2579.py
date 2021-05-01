6339a43ae269c280f340cf1217dcee3d4d2d877c
tpdns90321 <tpdns9032100@gmail.com>
2021-05-01 11:59:23 +0000

    Solve 2579

---
+++ 2579/2579.py
@@ -0,0 +1,21 @@
+from sys import stdin
+
+num_of_stair = int(stdin.readline())
+stairs = [int(stdin.readline()) for _ in range(num_of_stair)]
+stairs.insert(0, 0)
+scores = [0 for _ in range(num_of_stair + 1)]
+
+scores[1] = stairs[1]
+if num_of_stair > 1:
+    scores[2] = sum(stairs[1:3]);
+
+for i in range(3, num_of_stair+1):
+    scores[i] = max([
+        # jump
+        scores[i - 2] + stairs[i],
+        # continue
+        scores[i - 3] + sum(stairs[i-1:i+1])
+    ])
+
+print(scores[num_of_stair])
+
