1ba1f39e025c86d12ebdc010dd2dd95607dd3058
tpdns90321 <tpdns9032100@gmail.com>
2021-05-15 13:41:46 +0000

    Solve 7576

--- 7576/7576.py
+++ 7576/7576.py
@@ -1,4 +1,5 @@
 from sys import stdin
+from collections import deque
 from enum import Enum
 
 "https://www.acmicpc.net/problem/7576 토마토 <Silver I>"
@@ -14,7 +15,7 @@ def dispensing_tomato(state):
         return Tomato.EMPTY
 
 def ripening_of_tomato(box, total_x, total_y):
-    queue = []
+    queue = deque()
     visited = [[False for _ in range(total_x)] for _ in range(total_y)]
 
     for y in range(total_y):
@@ -26,27 +27,30 @@ def ripening_of_tomato(box, total_x, total_y):
                 visited[y][x] = True
 
     last_lapse = 0
-    while len(queue) != 0:
-        x, y, lapse = queue.pop()
-
-        diff = []
-        if x != 0:
-            diff.append((-1, 0))
-        if x != total_x - 1:
-            diff.append((1, 0))
-        if y != 0:
-            diff.append((0, -1))
-        if y != total_y - 1:
-            diff.append((0, 1))
-
-        for d in diff:
-            dx = x + d[0]
-            dy = y + d[1]
-            if not visited[dy][dx] and box[dy][dx] == Tomato.UNRIPE:
-                visited[dy][dx] = True
-                box[dy][dx] = Tomato.RIPE
-                last_lapse = lapse + 1
-                queue.insert(0, (dx, dy, last_lapse))
+    try:
+        while True:
+            x, y, lapse = queue.popleft()
+    
+            diff = []
+            if x != 0:
+                diff.append((-1, 0))
+            if x != total_x - 1:
+                diff.append((1, 0))
+            if y != 0:
+                diff.append((0, -1))
+            if y != total_y - 1:
+                diff.append((0, 1))
+    
+            for d in diff:
+                dx = x + d[0]
+                dy = y + d[1]
+                if not visited[dy][dx] and box[dy][dx] == Tomato.UNRIPE:
+                    visited[dy][dx] = True
+                    box[dy][dx] = Tomato.RIPE
+                    last_lapse = lapse + 1
+                    queue.append((dx, dy, last_lapse))
+    except IndexError:
+        pass
 
     yet_unripe = set()
     for y in visited:
@@ -55,7 +59,7 @@ def ripening_of_tomato(box, total_x, total_y):
         if len(yet_unripe) != 1:
             break
 
-    if len(yet_unripe) != 1:
+    if len(yet_unripe) != 1 or not yet_unripe.pop():
         return -1
 
     return last_lapse
