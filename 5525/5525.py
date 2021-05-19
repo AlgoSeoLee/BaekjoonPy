4d77e743353f67693b4d488d886067758de8f915
tpdns90321 <tpdns9032100@gmail.com>
2021-05-19 12:19:58 +0000

    Solve 5525

--- 5525/5525.py
+++ 5525/5525.py
@@ -2,43 +2,55 @@ from sys import stdin
 
 "https://www.acmicpc.net/problem/5525 IOIOI <Silver II>"
 
+def _calc_partial_match(search):
+    length_of_search = len(search)
+    partial_match = [0 for _ in range(length_of_search)]
+
+    begin = 1
+    match = 0
+    while begin + match < length_of_search:
+        if search[begin + match] == search[match]:
+            match += 1
+            partial_match[begin + match - 1] = match
+        elif match == 0:
+            begin += 1
+        else:
+            begin += match - partial_match[match - 1]
+            match = partial_match[match - 1]
+
+    return partial_match
+
+def kmp_search(plain, search):
+    length_of_plain = len(plain)
+    length_of_search = len(search)
+
+    partial_match = _calc_partial_match(search)
+    begin = 0
+    match = 0
+    result = 0
+    limit = length_of_plain - length_of_search
+    while begin <= limit:
+        is_same = match < length_of_search
+        is_same = is_same and plain[begin + match] == search[match]
+        if is_same:
+            match += 1
+
+            if match == length_of_search:
+                result += 1
+        elif match == 0:
+            begin += 1
+        else:
+            offset = partial_match[match - 1]
+            begin += match - offset
+            match = offset
+
+    return result
+
 num_of_O = int(stdin.readline())
 length_of_plain = int(stdin.readline())
 plain = stdin.readline()
 
-condition = 'I' + 'OI' * num_of_O
+search = 'I' + 'OI' * num_of_O
 
-level = 0
-maximum_level = len(condition) - 1
-multiple_O = num_of_O > 1
-result = 0
-started_at = 0
-position = 0
-while position < length_of_plain:
-    current = plain[position]
-    target = condition[level]
-    if current == target:
-        if multiple_O and level == 0:
-            started_at = position
-
-        if level == maximum_level:
-            level = 1
-            result += 1
-            if multiple_O:
-                started_at = started_at + 2
-                next_phase = started_at + maximum_level + 1
-                if next_phase > length_of_plain:
-                    break
-                position = started_at
-        else:
-            level += 1
-    elif current == 'I':
-        started_at = position
-        level = 1
-    else:
-        level = 0
-
-    position += 1
-
-print(result)
+print(kmp_search(plain, search))
 
