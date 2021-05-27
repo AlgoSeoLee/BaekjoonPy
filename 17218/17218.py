d1cfca94e5e19f26d6fc7106195ed1cf3276e364
tpdns90321 <tpdns9032100@gmail.com>
2021-05-27 08:59:06 +0000

    Solve 17218 with LCS algorithm

--- 17218/17218.py
+++ 17218/17218.py
@@ -1,82 +1,43 @@
 from sys import stdin
-from bisect import bisect_left
 
 "https://www.acmicpc.net/problem/17218 비밀번호 만들기 <Gold V>"
 
-def compressed_A_and_set_B(plain_A, plain_B):
-    compressed_A = ''
-    set_B = {}
+def longest_common_subsequence(plain_A, plain_B):
+    length_of_A = len(plain_A) + 1
+    length_of_B = len(plain_B) + 1
 
-    for current_A in plain_A:
-        is_duplicate = False
-        index_array_of_B = set_B.get(current_A)
-        if index_array_of_B:
-            is_duplicate = True
-            latest_index = index_array_of_B[-1]
-            if latest_index:
-                new_index = plain_B[latest_index+1:].index(current_A)
-                new_index += latest_index + 1
-                index_array_of_B.append(new_index)
-        else:
-            index_of_plain_B = None
-            try:
-                index_of_plain_B = plain_B.index(current_A)
-            except ValueError:
+    dp = []
+    for y in range(length_of_B):
+        dp.append([])
+        for x in range(length_of_A):
+            add = 0
+            if y == 0 or x == 0:
                 pass
+            elif plain_A[x - 1] == plain_B[y - 1]:
+                add = dp[y - 1][x - 1] + 1
+            else:
+                add = max(dp[y][x - 1], dp[y - 1][x])
 
-            if index_of_plain_B != None:
-                is_duplicate = True
-                set_B.update({current_A: [index_of_plain_B]})
-            
-        if is_duplicate:
-            compressed_A += current_A
+            dp[y].append(add)
 
-    for key_B in set_B:
-        index_array = set_B[key_B]
-        index = index_array[-1]
-        slice_B = plain_B[index+1:]
-        while True:
-            try:
-                index += slice_B.index(key_B) + 1
-                index_array.append(index)
-                slice_B = plain_B[index+1:]
-            except ValueError:
-                break
+    subsequence = ''
+    x = length_of_A - 1
+    y = length_of_B - 1
+    while dp[y][x] != 0:
+        current_score = dp[y][x]
+        if dp[y - 1][x] == current_score:
+            y -= 1
+        elif dp[y][x - 1] == current_score:
+            x -= 1
+        else:
+            subsequence = plain_A[x - 1] + subsequence
+            x -= 1
+            y -= 1
 
-
-    return (compressed_A, set_B)
-
-def generate_substring(compressed_A, set_B):
-    maximum_substring = ''
-    maximum_length = 0
-    for start_A in range(len(compressed_A)):
-        substring = ''
-        minimum_B = 0
-
-        for current_A in compressed_A[start_A:]:
-            index_B = bisect_left(set_B[current_A], minimum_B)
-            try: 
-                index_B = set_B[current_A][index_B]
-            except:
-                continue
-
-            if index_B > minimum_B or minimum_B == 0:
-                minimum_B = index_B
-                if minimum_B == 0:
-                    minimum_B += 1
-                substring += current_A
-
-        length_of_substring = len(substring)
-        if length_of_substring > maximum_length:
-            maximum_substring = substring
-            maximum_length = length_of_substring
-
-    return maximum_substring
+    return subsequence
 
 plain_A = stdin.readline().rstrip()
 plain_B = stdin.readline().rstrip()
 
-compressed_A, set_B = compressed_A_and_set_B(plain_A, plain_B)
-print(compressed_A, set_B)
-print(generate_substring(compressed_A, set_B))
+print(longest_common_subsequence(plain_A, plain_B))
 
