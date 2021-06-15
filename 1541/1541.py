d8ad5021d0794a3bae6974ce347dc7ec1427ef21
tpdns90321 <tpdns90321@users.noreply.github.com>
2021-06-15 13:30:42 +0000

    auto comment for '["1541/1541.py","1541/example1.in","1541/example2.in","1541/example3.in","1541/example4.in"]'

--- 1541/1541.py
+++ 1541/1541.py
@@ -2,6 +2,8 @@ from sys import stdin
 from collections import deque
 import re
 
+"https://www.acmicpc.net/problem/1541 잃어버린 괄호 <Silver II>"
+
 REGEXP = re.compile(r"[+-]")
 def solve_minimize(plain):
     operators = REGEXP.findall(plain)
