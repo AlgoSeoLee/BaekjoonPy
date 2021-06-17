c290631c031d1481f79f1b97e340016ad08277b9
tpdns90321 <tpdns9032100@gmail.com>
2021-06-17 16:00:34 +0900

    Solve 11286

---
+++ 11286/11286.py
@@ -0,0 +1,37 @@
+from sys import stdin
+from heapq import heappop, heappush
+
+class ABSHeap:
+    def __init__(self):
+        self.heap = []
+        self.elem = {}
+
+    def push(self, e):
+        heappush(self.heap, abs(e))
+        if not self.elem.get(e):
+            self.elem[e] = 0
+        self.elem[e] += 1
+
+    def pop(self):
+        try:
+            plus = heappop(self.heap)
+            minus = plus * -1
+            if self.elem.get(minus) != None and self.elem[minus] > 0:
+                self.elem[minus] -= 1
+                return minus
+            else:
+                self.elem[plus] -= 1
+                return plus
+        except IndexError:
+            pass
+
+        return 0
+
+num_of_case = int(stdin.readline())
+heap = ABSHeap()
+for _ in range(num_of_case):
+    elem = int(stdin.readline())
+    if elem == 0:
+        print(heap.pop())
+    else:
+        heap.push(elem)
