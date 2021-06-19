f513b805534027eccff98f128bd21ac34ee2d0e6
tpdns90321 <tpdns9032100@gmail.com>
2021-06-19 17:11:50 +0900

    Solve 2407

---
+++ 2407/2407.py
@@ -0,0 +1,18 @@
+from sys import stdin
+
+DP = {}
+def combination(case, select):
+    if case == select or select == 0:
+        return 1
+
+    result = DP.get((case, select))
+    if result != None:
+        return result
+
+    result = combination(case - 1, select - 1)
+    result += combination(case - 1, select)
+    DP[(case, select)] = result
+    return result
+
+case, select = map(int, stdin.readline().split())
+print(combination(case, select))
