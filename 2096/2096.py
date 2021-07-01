292462c46928ea9e802b024f74434711ecd76880
tpdns90321 <tpdns9032100@gmail.com>
2021-07-01 16:01:52 +0900

    Solve 2096

--- 2096/2096.py
+++ 2096/2096.py
@@ -2,51 +2,21 @@ from sys import stdin
 
 "https://www.acmicpc.net/problem/2096 내려가기 <Gold IV>"
 
-def init_dp(matrix, matrix_size):
-    dp = [
-        [0 for _ in range(matrix_size)]
-        for _ in range(matrix_size)
-    ]
-    for i in range(matrix_size):
-        dp[0][i] = matrix[0][i]
+num_of_line = int(stdin.readline())
 
-    return dp
+min_dp = [int(v) for v in stdin.readline().split()]
+max_dp = min_dp.copy()
 
-def calc_both(matrix_line, func, last, cur_line, prev_line, dp):
-    cur_line[0] = func(prev_line[0], prev_line[1])
-    cur_line[0] += matrix_line[0]
-    cur_line[last] = func(prev_line[last], prev_line[last-1])
-    cur_line[last] += matrix_line[last]
+def calc_dp(func, dp, prev, line):
+    dp[0] = line[0] + func(prev[0:2])
+    dp[1] = line[1] + func(prev[0:3])
+    dp[2] = line[2] + func(prev[1:3])
 
-def calc_dp(matrix_line, func, cur_line, prev_line, index, dp):
-    value = matrix_line[index]
-    value += func(
-        prev_line[index - 1],
-        prev_line[index],
-        prev_line[index+1]
-    )
-    cur_line[index] = value
+for _ in range(1, num_of_line):
+    line = [int(v) for v in stdin.readline().split()]
+    min_prev = min_dp.copy()
+    max_prev = max_dp.copy()
+    calc_dp(max, max_dp, max_prev, line)
+    calc_dp(min, min_dp, min_prev, line)
 
-matrix_size = int(stdin.readline())
-matrix = [
-    list(map(int, stdin.readline().split()))
-    for _ in range(matrix_size)
-]
-dp = init_dp(matrix, matrix_size)
-
-def run_dp(func):
-    last = matrix_size - 1
-    for line_num in range(1, matrix_size):
-        cur_line = dp[line_num]
-        prev_line = dp[line_num - 1]
-        matrix_line = matrix[line_num]
-
-        calc_both(matrix_line, func, last, cur_line, prev_line, dp)
-
-        for index in range(1, last):
-            calc_dp(matrix_line, func, cur_line, prev_line, index, dp)
-
-    return func(dp[last])
-
-print(run_dp(max), run_dp(min))
-
+print(max(max_dp), min(min_dp))
