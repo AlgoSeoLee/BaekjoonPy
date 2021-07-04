395fc858bcc6eb84c0e2eb5ef5a571f62bdd9972
tpdns90321 <tpdns9032100@gmail.com>
2021-07-04 13:52:15 +0000

    Solve 11659

--- 11659/11659.py
+++ 11659/11659.py
@@ -1,19 +1,16 @@
 from sys import stdin
+from itertools import accumulate
 
 "https://www.acmicpc.net/problem/11659 구간 합 구하기 4 <Silver III>"
 
-def get_integers():
-    return map(int, stdin.readline().split())
-
-_, num_of_question = get_integers()
-
-numbers = stdin.readline().split()
-for _ in range(num_of_question):
-    start, end = get_integers()
-    start -= 1
-
-    result = 0
-    for num in numbers[start:end]:
-        result += int(num)
+_, num_of_answer = map(int, stdin.readline().split())
+element = map(int, stdin.readline().split())
+prefix = list(accumulate(element, lambda a, x: a + x))
+for _ in range(num_of_answer):
+    x, y = map(lambda x: int(x) - 1, stdin.readline().split())
+    result = prefix[y]
+    if x != 0:
+        result -= prefix[x - 1]
 
     print(result)
+
