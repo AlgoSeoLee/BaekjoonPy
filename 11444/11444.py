a6c067e6cde89254a7373824f3ea41b52a6b5b0c
tpdns90321 <tpdns9032100@gmail.com>
2021-07-05 12:28:25 +0000

    Solve 11444

---
+++ 11444/11444.py
@@ -0,0 +1,31 @@
+from sys import stdin
+
+MOD = 1000000007
+
+def matrix_mul(size, a, b):
+    c = [[0 for _ in range(size)] for _ in range(size)]
+    for i in range(size):
+        for j in range(size):
+            for k in range(size):
+                c[i][j] += a[i][k] * b[k][j]
+            c[i][j] %= MOD
+
+    return c
+
+def fibo(times):
+    if times <= 0:
+        return 0
+    elif times <= 2:
+        return 1
+
+    answer = [[1, 0], [0, 1]]
+    acc = [[1, 1], [1, 0]]
+    while times > 0:
+        if times % 2 == 1:
+            answer = matrix_mul(2, answer, acc)
+        acc = matrix_mul(2, acc, acc)
+        times //= 2
+
+    return answer[0][1]
+
+print(fibo(int(stdin.readline())))
