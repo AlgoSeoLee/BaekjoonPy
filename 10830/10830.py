a5c986d82601a73bf528a55cc55bd3d72bdaca12
tpdns90321 <tpdns9032100@gmail.com>
2021-07-06 06:41:47 +0000

    Solve 10830

---
+++ 10830/10830.py
@@ -0,0 +1,30 @@
+from sys import stdin
+
+MOD = 1000
+def matrix_mul(size, a, b):
+    c = [[0 for _ in range(size)] for _ in range(size)]
+    for i in range(size):
+        for j in range(size):
+            for k in range(size):
+                c[i][j] += a[i][k] * b[k][j]
+            c[i][j] %= MOD
+
+    return c
+
+size, times = map(int, stdin.readline().split())
+matrix = [[int(v) for v in stdin.readline().split()] for _ in range(size)]
+result = [[v % 1000 for v in line] for line in matrix]
+times -= 1
+
+while times > 0:
+    if times % 2 == 1:
+        result = matrix_mul(size, result, matrix)
+
+    matrix = matrix_mul(size, matrix, matrix)
+    times //= 2
+
+for i in range(size):
+    for j in range(size):
+        print(result[i][j], end=' ')
+    print()
+
