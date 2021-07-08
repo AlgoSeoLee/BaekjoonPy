2423dab9a97b67594c5a8e7fa538ffb794008417
tpdns90321 <tpdns9032100@gmail.com>
2021-07-08 10:31:37 +0000

    Solve 12851

--- 12851/12851.py
+++ 12851/12851.py
@@ -4,48 +4,43 @@ from collections import deque
 "https://www.acmicpc.net/problem/12851 숨바꼭질 2 <Gold V>"
 
 def BFS(start, target):
-    visited = {}
-    visited[start] = True
     queue = deque()
     queue.appendleft((start, 0))
-    startFromOne = start == 1
+    visited = {}
+    visited[start] = 1
 
-    result = []
+    min_level = 100000
+    method = 0
+
+    ops = [
+        lambda a: a + 1,
+        lambda a: a - 1,
+        lambda a: a * 2
+    ]
+
     while queue:
         current, level = queue.pop()
         if current == target:
-            result.append(level)
+            if level == min_level:
+                method += 1
+            elif level < min_level:
+                min_level = level
+                method = 1
             continue
 
-        ways = []
-        plus = current + 1
-        minus = current - 1
-        double = current * 2
-
-        if startFromOne and plus == double:
-            dup = True
-            ways.append(plus)
-        else:
-            ways.append(plus)
-            ways.append(double)
-
-        ways.append(minus)
+        ways = map(lambda f: f(current), ops)
         ways = filter(lambda w: w >= 0 and w <= 100000, ways)
 
         next_level = level + 1
         for n in ways:
-            if not visited.get(n):
+            if not visited.get(n) or visited[n] >= next_level:
                 if n != target:
-                    visited[n] = True
+                    visited[n] = next_level
                 queue.appendleft((n, next_level))
 
-    if startFromOne:
-        result.extend(result)
-
-    return result
+    return min_level, method
 
 subin, brother = map(int, stdin.readline().split())
-result = BFS(subin, brother)
-print(min(result))
-print(len(result))
-
+level, method = BFS(subin, brother)
+print(level)
+print(method)
