514d9b9c71e314a811ca7ce4e56ea43a72636126
tpdns90321 <tpdns9032100@gmail.com>
2021-07-29 16:38:53 +0900

    performance improvement: 13549
    문제를 해결한 후 다른 정답들을 보다 더 좋은 방법이 나와서 수정합니다.
    기존해결 방식은 heapq 라이브러리를 활용한 heap 큐를 활용해 우선순위를
    알아내서 사용했는데 자료를 넣고 뺄때 순서를 계산하기 때문에 손해가
    발생했다. 하지만 deque의 앞뒤로 삽입이 용이하다는 점을 이용한 해결방식은
    순서를 계산할 필요없이 곱하기는 앞, 나머지 연산은 뒤에 배치해서
    자연스럽게 곱하기가 먼저나오는 queue를 손쉽게 구현할 수 있다.

--- 13549/13549.py
+++ 13549/13549.py
@@ -1,5 +1,5 @@
 from sys import stdin
-from heapq import heappop, heappush
+from collections import deque
 
 "https://www.acmicpc.net/problem/13549 숨바꼭질 3 <Gold V>"
 
@@ -7,12 +7,12 @@ MAXIMUM = 100000
 MINIMUM = 0
 
 def finding_shortest_time(start, target):
-    queue = []
+    queue = deque()
     queue.append((0, start))
     visited = [False for _ in range(MAXIMUM + 1)]
 
     while queue:
-        current_cost, current = heappop(queue)
+        current_cost, current = queue.popleft()
 
         if current == target:
             return current_cost
@@ -21,18 +21,15 @@ def finding_shortest_time(start, target):
         minus = current - 1
         plus = current + 1
 
-        ways = []
-        if double > MINIMUM and double <= MAXIMUM :
-            ways.append((current_cost, double))
-        if plus >= MINIMUM and plus <= MAXIMUM:
-            ways.append((current_cost + 1, plus))
-        if minus >= MINIMUM and minus <= MAXIMUM:
-            ways.append((current_cost + 1, minus))
-
-        for w in ways:
-            if not visited[w[1]]:
-                visited[w[1]] = True
-                heappush(queue, w)
+        if double > MINIMUM and double <= MAXIMUM and not visited[double]:
+            visited[double] = True
+            queue.appendleft((current_cost, double))
+        if plus <= MAXIMUM and not visited[plus]:
+            visited[plus] = True
+            queue.append((current_cost + 1, plus))
+        if minus >= MINIMUM and not visited[minus]:
+            visited[minus] = True
+            queue.append((current_cost + 1, minus))
 
 start, target = map(int, stdin.readline().split())
 print(finding_shortest_time(start, target))
