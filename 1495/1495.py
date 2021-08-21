83ec78a140798047efe5430a02ea86f4f7e94ba7
tpdns90321 <tpdns9032100@gmail.com>
2021-08-21 21:37:39 +0900

    Solve 1495

--- 1495/1495.py
+++ 1495/1495.py
@@ -1,38 +1,32 @@
 from sys import stdin
-from collections import deque
 
 read_integers = lambda: map(int, stdin.readline().split())
 
 num_of_song, start_volume, maximum_volume = read_integers()
-volumes = deque(read_integers())
-volumes.appendleft(start_volume)
-dp = [[start_volume, start_volume, start_volume]]
+volumes = list(read_integers())
+dp = [[start_volume]]
 
 for index in range(1, num_of_song + 1):
     prev = index - 1
 
-    prev_max_volume = max(dp[prev])
-    prev_min_volume = min(dp[prev])
-    cur_volume = volumes[index]
-
-    current = [0, 0]
-
-    current = [
-        prev_min_volume - cur_volume,
-        prev_max_volume + cur_volume,
-        prev_max_volume - cur_volume
-    ]
-    if current[1] > maximum_volume:
-        current[1] = prev_min_volume + cur_volume
-
-    if current[0] < 0:
-        current[0] = prev_max_volume - cur_volume
-
+    current = set()
+    current_volume = volumes[prev]
+    for prev_volume in dp[prev]:
+        add = [
+            prev_volume + current_volume,
+            prev_volume - current_volume
+        ]
+        current.update(
+            filter(lambda v: v <= maximum_volume and v >= 0, add)
+        )
+        
     dp.append(current)
 
-result = max(dp[num_of_song])
-if result < 0 or result > maximum_volume:
+try:
+    result = max(dp[num_of_song])
+    if result < 0 or result > maximum_volume:
+        result = -1
+except:
     result = -1
-print(dp)
 print(result)
 
