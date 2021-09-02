4239be5032bc8ca9ca872b63d7465f8352d86374
tpdns90321 <tpdns9032100@gmail.com>
2021-09-02 21:42:36 +0900

    Failed 14171

--- 14171/14171.py
+++ 14171/14171.py
@@ -1,15 +1,25 @@
 from sys import stdin
 
-pairs = {}
+"https://www.acmicpc.net/problem/14171 Cities and States <Silver I>"
+
+diff = {}
 
 num_of_city = int(stdin.readline())
 result = 0
 for _ in range(num_of_city):
-    state, code = stdin.readline().split()
-    if pairs.get(code):
-        result += 1
+    city, state = stdin.readline().split()
+    start_of_city = city[:2]
 
-    pairs[code] = state
+    if state != start_of_city:
+        same_start_city = diff.get(start_of_city)
+        if same_start_city and state in same_start_city:
+            result += 1
+
+    same_state = diff.get(state)
+    if same_state == None:
+        same_state = set()
+        diff[state] = same_state
+    same_state.add(start_of_city)
 
 print(result)
 
