f3a160b59ccaec00d88103765b6d49f32cbb5f4b
tpdns90321 <tpdns9032100@gmail.com>
2021-09-04 21:01:35 +0900

    Solve 14171

--- 14171/14171.py
+++ 14171/14171.py
@@ -2,24 +2,22 @@ from sys import stdin
 
 "https://www.acmicpc.net/problem/14171 Cities and States <Silver I>"
 
-diff = {}
-
 num_of_city = int(stdin.readline())
+
+same = {}
 result = 0
 for _ in range(num_of_city):
     city, state = stdin.readline().split()
-    start_of_city = city[:2]
+    city = city[:2]
 
-    if state != start_of_city:
-        same_start_city = diff.get(start_of_city)
-        if same_start_city and state in same_start_city:
-            result += 1
+    if city == state:
+        continue
 
-    same_state = diff.get(state)
-    if same_state == None:
-        same_state = set()
-        diff[state] = same_state
-    same_state.add(start_of_city)
+    if same.get((city, state)) == None:
+        same[(city, state)] = 0
+    same[(city, state)] += 1
+
+    if same.get((state, city)) != None:
+        result += same[(state, city)]
 
 print(result)
-
