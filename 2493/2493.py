a76e65067abd08b4eb68bdd058edf4b68a57427f
tpdns90321 <tpdns9032100@gmail.com>
2021-09-06 20:57:37 +0900

    Solve 2493

---
+++ 2493/2493.py
@@ -0,0 +1,24 @@
+from sys import stdin
+
+num_of_tower = int(stdin.readline())
+towers = [int(v) for v in stdin.readline().split()]
+
+received = [0 for _ in range(num_of_tower)]
+unreceive = []
+for index in range(num_of_tower - 1, -1, -1):
+    current = towers[index]
+    while unreceive:
+        start_index = unreceive.pop()
+        start = towers[start_index]
+        if current > start:
+            received[start_index] = index + 1
+        else:
+            unreceive.append(start_index)
+            break
+
+    unreceive.append(index)
+
+for to in received:
+    print(to, end=" ")
+print()
+
