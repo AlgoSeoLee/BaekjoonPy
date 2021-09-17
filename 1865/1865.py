e6df49502ad8920c8d716e05080331f1e1f5ad96
tpdns90321 <tpdns9032100@gmail.com>
2021-09-17 15:56:24 +0900

    Solve 1865

--- 1865/1865.py
+++ 1865/1865.py
@@ -11,28 +11,29 @@ class Edge:
 def BellmanFord(edges, num_of_edge, num_of_node, source):
     distance = [maxsize if i != source else 0 for i in range(num_of_node)]
 
-    for _ in range(num_of_node):
+    for node in range(num_of_node):
         for e in edges:
             relaxing = distance[e.source] + e.weight
-            if distance[e.destination] > relaxing:
+            if relaxing < distance[e.destination]:
                 distance[e.destination] = relaxing
-
-    for e in edges:
-        if distance[e.destination] > distance[e.source] + e.weight:
-            return True
+                if node == num_of_node - 1:
+                    return True
 
     return False
 
 def input_integers():
     return map(int, stdin.readline().split())
 
-def input_edges(num_of_edge, is_negative=False):
+def input_edges(num_of_edge, is_negative=False, is_bidirection=False):
     edges = []
     for _ in range(num_of_edge):
         src, dst, weight = input_integers()
         if is_negative:
             weight = -weight
+
         edges.append(Edge(src, dst, weight))
+        if is_bidirection:
+            edges.append(Edge(dst, src, weight))
 
     return edges
 
@@ -41,8 +42,8 @@ for _ in range(num_of_case):
     num_of_node, num_of_road, num_of_wormhole = input_integers()
     edges = []
 
-    edges.extend(input_edges(num_of_road))
-    edges.extend(input_edges(num_of_wormhole, True))
+    edges.extend(input_edges(num_of_road, is_bidirection=True))
+    edges.extend(input_edges(num_of_wormhole, is_negative=True))
     if BellmanFord(edges, num_of_road + num_of_wormhole, num_of_node, 0):
         print("YES")
     else:
