50463bb20d439e099438fc46c8cc441ef56924d6
tpdns90321 <tpdns9032100@gmail.com>
2021-10-03 20:52:44 +0900

    Failed 1520

--- 1520/1520.py
+++ 1520/1520.py
@@ -1,5 +1,4 @@
 from sys import stdin
-from collections import deque
 
 read_integers = lambda: map(int, stdin.readline().split())
 
@@ -16,42 +15,50 @@ DIRECTIONS = [
     (0, -1)
 ]
 
-def solve_find_downhill_ways(num_of_row, num_of_column, matrix):
-    queue = deque()
-    visited = {}
+def _DFS(matrix, visited, num_of_row, num_of_column, row, column):
+    get_height = lambda r, c: matrix[r][c]
+    is_visited = lambda r, c: visited[r][c]
 
-    start_height = matrix[0][0]
-    queue.append((0, 0, start_height))
+    condition = (row == num_of_row - 1 and column == num_of_column - 1)
+    condition = condition or is_visited(row,column)
+    if condition:
+        return 1
 
-    result = 0
-    endpoint = (
-        num_of_column - 1,
-        num_of_row - 1
+    height_when_entered = get_height(row, column)
+
+    ways = map(
+        lambda d: (row + d[0], column + d[1]),
+        DIRECTIONS
     )
+    ways = filter(
+        lambda w:
+            w[0] >= 0 and w[0] < num_of_row and
+            w[1] >= 0 and w[1] < num_of_column and
+            is_visited(w[0], w[1]) != -1,
+        ways
+    )
+    ways = list(map(lambda w: (get_height(w[0], w[1]), w[0], w[1]), ways))
+    ways.sort()
 
-    get_height = lambda w: matrix[w[1]][w[0]]
+    visited[row][column] = -1
+    acc = 0
+    for w in ways:
+        h, r, c = w
+        if h < height_when_entered:
+            print(w)
+            acc += _DFS(matrix, visited, num_of_row, num_of_column, r, c)
+        else:
+            break
+    visited[row][column] = acc
+    return acc
 
-    while queue:
-        current = queue.pop()
-        x, y, total = current
-
-        ways = map(lambda d: (x + d[0], y + d[1]), DIRECTIONS)
-        ways = filter(lambda w: w[0] >= 0 and w[0] < num_of_column, ways)
-        ways = filter(lambda w: w[1] >= 0 and w[1] < num_of_row, ways)
-        ways = list(map(
-            lambda w: (w[0], w[1], total + get_height(w)),
-            ways
-        ))
-        ways = filter(lambda w: not visited.get(w), ways)
-        ways = filter(lambda w: get_height(w) < get_height(current), ways)
-
-        for w in ways:
-            if w[:2] == endpoint:
-                result += 1
-            else:
-                queue.appendleft(w)
-                visited[w] = True
-
+def solve_find_downhill_ways(num_of_row, num_of_column, matrix):
+    visited = [
+        [0 for _ in range(num_of_column)]
+        for _ in range(num_of_row)
+    ]
+    visited[0][0] = 0
+    result = _DFS(matrix, visited, num_of_row, num_of_column, 0, 0)
     return result
 
 num_of_row, num_of_column = read_integers()
