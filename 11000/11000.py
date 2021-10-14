342e4f7e7ab589e96c8cf0a54bdbad34e69e7bcb
tpdns90321 <tpdns9032100@gmail.com>
2021-10-14 19:55:47 +0900

    Solve 11000

--- 11000/11000.py
+++ 11000/11000.py
@@ -1,5 +1,7 @@
 from sys import stdin
-from collections import deque
+from heapq import heappush, heappop
+
+"https://www.acmicpc.net/problem/11000 강의실 배정 <Gold V>"
 
 num_of_lecture = int(stdin.readline())
 
@@ -8,36 +10,19 @@ for _ in range(num_of_lecture):
     lectures.append(tuple(int(hour) for hour in stdin.readline().split()))
 lectures.sort()
 
-classes = deque()
-num_of_class = 0
-for lec_start, lec_end in lectures:
-    if num_of_class == 0:
-        num_of_class += 1
-        classes.append(lec_end)
+new_classroom = lambda lec: tuple(reversed(lec))
+
+classrooms = [new_classroom(lectures[0])]
+num_of_classroom = 1
+for lec in lectures[1:]:
+    lec_start, _ = lec
+    classroom_end, _ = classrooms[0]
+
+    if classroom_end > lec_start:
+        num_of_classroom += 1
+        heappush(classrooms, new_classroom(lec))
     else:
-        is_replace = False
-        is_complete = False
-        for class_idx in range(num_of_class - 1):
-            current = classes[class_idx]
-            if current <= lec_start:
-                classes.remove(current)
-                is_replace = True
+        heappop(classrooms)
+        heappush(classrooms, new_classroom(lec))
 
-            if is_replace and classes[class_idx] >= lec_end:
-                classes.insert(class_idx, lec_end)
-                is_complete = True
-                break
-
-        if not is_complete:
-            position = num_of_class - 1
-            if is_replace:
-                position -= 1
-
-            if classes[position] > lec_end:
-                classes.insert(position, lec_end)
-            else:
-                if not is_replace:
-                    num_of_class += 1
-                classes.append(lec_end)
-
-print(num_of_class)
+print(num_of_classroom)
