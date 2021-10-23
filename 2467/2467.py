d2bd7a5bd82a81ed2393317ca8d39b82c531e0c9
tpdns90321 <tpdns9032100@gmail.com>
2021-10-23 18:35:31 +0900

    Solve 2467

---
+++ 2467/2467.py
@@ -0,0 +1,26 @@
+from sys import stdin, maxsize
+
+num_of_solution = int(stdin.readline())
+solutions = [int(v) for v in stdin.readline().split()]
+
+base = 0
+additive = num_of_solution - 1
+
+closest_mix = maxsize
+closest_base = 0
+closest_additive = num_of_solution - 1
+while base != additive:
+    mix = solutions[base] + solutions[additive]
+    if abs(mix) <= closest_mix:
+        closest_mix = abs(mix)
+        closest_base = base
+        closest_additive = additive
+
+    if mix < 0:
+        base += 1
+    elif mix > 0:
+        additive -= 1
+    else:
+        break
+
+print(solutions[closest_base], solutions[closest_additive])
