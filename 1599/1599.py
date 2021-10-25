e50fdf645b621055b4846d79a45f6b404754922b
tpdns90321 <tpdns9032100@gmail.com>
2021-10-25 23:58:56 +0900

    Solve 1599

---
+++ 1599/1599.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+# N is ng
+mansik_char = "a b k d e g h i l m n N o p r s t u w y".split()
+mansik_idx = {
+    c: idx for c, idx in zip(mansik_char, range(len(mansik_char)))
+}
+
+words = []
+for _ in range(int(stdin.readline())):
+    words.append(stdin.readline().rstrip().replace("ng",'N'))
+
+result = []
+for w in words:
+    word_idx = tuple(mansik_idx[c] for c in w)
+    result.append((word_idx, w))
+
+for _, w in sorted(result):
+    print(w.replace('N', "ng"))
