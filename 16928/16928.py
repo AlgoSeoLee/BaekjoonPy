3b590d9b4a1a78910ea69328d35755b4d60afde1
tpdns90321 <tpdns9032100@gmail.com>
2022-02-15 22:42:49 +0900

    Solve 16928

---
+++ 16928/16928.py
@@ -0,0 +1,49 @@
+from sys import stdin
+from collections import deque
+
+num_of_ladder, num_of_snake = map(int, stdin.readline().split())
+
+ladder = {}
+snakes = {}
+
+def read_targets(num_of_target):
+    targets = {}
+    for _ in range(num_of_target):
+        start, to = map(int, stdin.readline().split())
+        targets[start] = to
+
+    return targets
+
+ladder = read_targets(num_of_ladder)
+snakes = read_targets(num_of_snake)
+
+queue = deque([[1, 0]])
+visited = {}
+while queue:
+    current, dice = queue.pop()
+
+    if current == 100:
+        print(dice)
+        break
+
+    find_snake = snakes.get(current)
+    if find_snake:
+        current = find_snake
+
+    find_ladder = ladder.get(current)
+    if find_ladder:
+        current = find_ladder
+
+    if visited.get(current):
+        continue
+
+    visited[current] = True
+
+    new_dice = dice + 1
+    for i in range(1, 7):
+        new_target = current + i
+        if new_target > 100:
+            break
+
+        queue.appendleft([new_target, new_dice])
+
