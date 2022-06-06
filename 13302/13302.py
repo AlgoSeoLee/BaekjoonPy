78a9ac67212798fdbac03735d7a5a15a2b4045b8
tpdns90321 <tpdns9032100@gmail.com>
2022-06-06 14:45:43 +0900

    Cant Solve 13302: Score 28

--- 13302/13302.py
+++ 13302/13302.py
@@ -1,19 +1,34 @@
-from sys import stdin
+from sys import stdin, maxsize
+from heapq import heappop, heappush
 
-def calc_minimum_resort_cost(rest_day, coupon):
-    if len(rest_day) == 0:
-        return 0
-    elif rest_day[0]:
-        return min([
-            37 + calc_minimum_resort_cost(rest_day[5:], coupon + 2),
-            25 + calc_minimum_resort_cost(rest_day[3:], coupon + 1),
-            10 + calc_minimum_resort_cost(rest_day[1:], coupon) if coupon < 3 else calc_minimum_resort_cost(rest_day[1:], coupon - 3)
-        ])
-    return calc_minimum_resort_cost(rest_day[1:], coupon)
+visited = {}
+def calc_minimum_resort_cost(days, num_of_days):
+    visited = {}
+    queue = [(0, 0, 0)]
+
+    while queue:
+        current = heappop(queue)
+        if visited.get(current):
+            continue
+
+        idx, cost, coupon = current
+        if idx >= num_of_days:
+            return cost
+        elif not days[idx]:
+            heappush(queue, (idx + 1, cost, coupon))
+
+        visited[current] = True
+
+        heappush(queue, (idx + 5, cost + 37, coupon + 2))
+        heappush(queue, (idx + 3, cost + 25, coupon + 1))
+        if coupon > 2:
+            heappush(queue, (idx + 1, cost, coupon - 3))
+        else:
+            heappush(queue, (idx + 1, cost + 10, coupon))
 
 num_of_days, _ = map(int, stdin.readline().split())
 days = [True for _ in range(num_of_days)]
 for schedule in map(int, stdin.readline().split()):
     days[schedule - 1] = False
 
-print(calc_minimum_resort_cost(days, 0))
+print(calc_minimum_resort_cost(days, num_of_days) * 1000)
