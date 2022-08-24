4901f1037f0ff178067e8eaff4e79888fb0f7c72
tpdns90321 <tpdns9032100@gmail.com>
2022-08-24 17:59:53 +0900

    Solved 2668

--- 2668/2668.py
+++ 2668/2668.py
@@ -2,55 +2,27 @@ from sys import stdin
 
 numbers = [int(stdin.readline()) for _ in range(int(stdin.readline()))]
 
-stack = [[set([i + 1]), set([j])] for i, j in enumerate(numbers)]
+maximum_comb = set()
 
-maximum_combination = set()
-
+stack = [[i] for i in range(len(numbers), 0, -1)]
 while stack:
-    ordered, random = stack.pop()
-
-    at_ordered = random - ordered
-    at_random = ordered - random
-
-    next_targets = []
-
-    if not at_ordered and not at_random:
-        same_combination = set()
-        for zero_index, number_of_random in enumerate(numbers):
-            number_of_ordered = zero_index + 1
-            if number_of_random == number_of_ordered:
-                same_combination.add(number_of_ordered)
-
-        combination = ordered | same_combination
-
-        if len(combination) > len(maximum_combination):
-            maximum_combination = combination
+    cycle = stack.pop()
+    visited = set(cycle[:-1])
+    latest = cycle[-1]
+    if latest in visited:
+        start = 0
+        for index in range(len(numbers)):
+            start = index
+            if cycle[index] == latest:
+                break
+        maximum_comb |= set(cycle[start:])
         continue
 
-    if at_random:
-        for zero_index, number_of_random in enumerate(numbers):
-            if number_of_random in at_random:
-                number_of_ordered = zero_index + 1
-                next_targets.append(
-                        [
-                            ordered | set([number_of_ordered]),
-                            random | set([number_of_random])
-                            ]
-                        )
+    next_of_cycle = numbers[latest - 1]
+    cycle.append(next_of_cycle)
 
-    if at_ordered:
-        for number_of_ordered in at_ordered:
-            zero_index = number_of_ordered - 1
-            target_random = numbers[zero_index]
-            next_targets.append(
-                    [
-                        ordered | set([number_of_ordered]),
-                        random | set([target_random])
-                    ]
-            )
+    stack.append(cycle)
 
-    stack.extend(next_targets)
-
-print(len(maximum_combination))
-for num in maximum_combination:
-    print(num)
+print(len(maximum_comb))
+for element in sorted(maximum_comb):
+    print(element)
